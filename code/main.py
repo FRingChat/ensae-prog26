28115fb033a034adde3cfe35f472b0693201b2d5
FRingChat <fringuetchatelier@gmail.com>
2026-04-12 16:30:22 +0200

    Dernières modifications et tests

--- code/main.py
+++ code/main.py
@@ -10,7 +10,8 @@ sys.path.append(str(ROOT / "code"))
 
 NET_DIR = ROOT / "examples"
 
-from network import Network  # noqa: E402
+from network import Network
+
 # --------------------------------------------------------
 
 # Load the network
@@ -20,12 +21,15 @@ network = Network.from_file(network_file)
 
 test1 = network.build_extended_graph()
 test2 = network.build_simple_graph()
-print(network.longueur_chemin(['lozère']))
-print(test1.shortest_path('lozere', 'saclay'))
+# print(network.longueur_chemin(['lozère']))
+# print(test1.shortest_path('lozere', 'saclay'))
 
 
 network_file = NET_DIR / "large-largefatigue.txt"
 network = Network.from_file(network_file)
 
 test = Network.from_file("examples/large-largefatigue.txt")
-print(test.A_etoile('v0', 'v12'))
+# print(test.A_etoile('v0', 'v12'))
+
+missions = [('v0', 'v12'), ('v14', 'v19'), ('v39', 'v45')]
+print(test.missions_multiples(missions))
